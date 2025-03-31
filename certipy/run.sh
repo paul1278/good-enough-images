59302b1303459db4f95e10f9b8a5c76947a8f3aa
/dev/paul <git@dev-paul.de>
2025-03-31 07:24:38 +0200

    Remove containers after run

--- certipy/run.sh
+++ certipy/run.sh
@@ -3,4 +3,4 @@ PARAM="$@"
 if [ -z "$PARAM" ]; then
     PARAM="bash"
 fi
-sudo docker run -i -t --network host -v /opt/good-enough-images/certipy:/main ghcr.io/paul1278/good-enough-images:certipy $PARAM
+sudo docker run --rm -i -t --network host -v /opt/good-enough-images/certipy:/main ghcr.io/paul1278/good-enough-images:certipy $PARAM
--- metasploit/run.sh
+++ metasploit/run.sh
@@ -3,4 +3,4 @@ PARAM="$@"
 if [ -z "$PARAM" ]; then
     PARAM="bash"
 fi
-sudo docker run -i -t --network host -v /opt/good-enough-images/msf:/root/.msf4 -v /opt/good-enough-images/msf/workdir:/main ghcr.io/paul1278/good-enough-images:msf $PARAM
+sudo docker run -i -t --rm  --network host -v /opt/good-enough-images/msf:/root/.msf4 -v /opt/good-enough-images/msf/workdir:/main ghcr.io/paul1278/good-enough-images:msf $PARAM
